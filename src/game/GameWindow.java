4a275c9d423d5b058fdc753df434953f7e693bfd
vukhanh09 <vxkhanh09@gmail.com>
2020-10-17 22:33:08 +0700

    build board

---
+++ src/game/GameWindow.java
@@ -0,0 +1,23 @@
+package game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameWindow extends JFrame {
+
+    public GameWindow(){
+        GamePanel gp = new GamePanel();
+        this.add(gp);
+        this.setTitle("Reversi v0.1");
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        //this.setSize(500,500);
+    }
+
+    public static void main(String[] args) {
+
+        new GameWindow();
+    }
+
+}
