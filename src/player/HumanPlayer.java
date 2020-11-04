0d457a5e0fa545204635d2b137d92cd257c12d06
vukhanh09 <vxkhanh09@gmail.com>
2020-11-04 21:18:08 +0700

    add

--- src/player/HumanPlayer.java
+++ src/player/HumanPlayer.java
@@ -4,7 +4,7 @@ import game.GamePlayer;
 
 import java.awt.*;
 
-public class HumanPlayer extends GamePlayer {
+public class  HumanPlayer extends GamePlayer {
 
     public HumanPlayer(int mark) {
         super(mark);
